fb6509359665b5447fd49f4d572bd6fad8733ab7
nekagit <nenad99@hotmail.de>
2024-03-11 15:50:35 +0100

    init

--- frontend/src/views/ProductPage.vue
+++ frontend/src/views/ProductPage.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import DataTable from '@/components/organisms/Tables/DataTable.vue';
+import  Button  from "@/components/ui/button/Button.vue";
+import type { IProduct } from '@/interfaces/atoms/IProduct';
 import { useProductStore } from '@/stores/productsStore';
-
+import { ref } from 'vue';
 // Watch for changes in the product store
  useProductStore().onInit()
-const products = useProductStore().products
-console.log(products)
+const products = ref(useProductStore().products)
+console.log(products.value)
+const handleCreate = () => {
+  useProductStore().createProduct({name: "asdf", unitPrice: 123} as IProduct)
+} 
 </script>
 
 <template>
@@ -13,7 +18,7 @@ console.log(products)
     <div class="flex items-center justify-between space-y-2">
       <div>
         <h2 class="text-2xl font-bold tracking-tight">
-          Customer Management
+          Product Management
         </h2>
         <p class="text-muted-foreground">
           Table over Customer Datas
@@ -22,6 +27,7 @@ console.log(products)
       <div class="flex items-center space-x-2">
       </div>
     </div>
+    <Button @click="handleCreate">Create</Button>
     <DataTable :data="products"  />
     <!-- <DataTable :data="customers"  />
     <DataTable :data="invoices"  /> -->
