ba0bda57c437746235675fc76193df979812131a
nekagit <nenad99@hotmail.de>
2024-03-12 13:25:42 +0100

    init

---
+++ frontend/src/components/organisms/Tables/Custom/ProductTable.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+import { useProductStore } from '@/stores/productsStore';
+import { ref } from 'vue';
+
+  const showDialog = ref(false)
+
+useProductStore().onInit()
+const localProducts = ref(useProductStore().products)
+
+
+
+</script>
+
+<template>
+    <DataTable :data="localProducts" />
+    <CreateDialog />
+    <EditDialog />
+</template>
+
--- frontend/src/views/ProductPage.vue
+++ frontend/src/views/ProductPage.vue
@@ -18,28 +18,6 @@ import { useForm } from 'vee-validate'
 import { ref } from 'vue'
 import * as z from 'zod'
 
-useProductStore().onInit()
-const localProducts = ref(useProductStore().products)
-
-const formSchema = toTypedSchema(
-  z.object({
-    name: z.string().min(2).max(50)
-  })
-)
-
-const { handleSubmit } = useForm({
-  validationSchema: formSchema
-})
-
-const onSubmit = handleSubmit(async (values) => {
-  localProducts.value = await useProductStore().createProduct({
-    name: values.name,
-    unitPrice: values.unitPrice,
-    status: EntityStatus.Created,
-    entityKey: AppModule.Product
-  } as IProduct)
-  console.log(localProducts.value.map((x) => x._id))
-})
 </script>
 
 <template>
@@ -51,30 +29,6 @@ const onSubmit = handleSubmit(async (values) => {
       </div>
       <div class="flex items-center space-x-2"></div>
     </div>
-    <form class="w-2/3 space-y-6" @submit="onSubmit">
-      <FormField v-slot="{ componentField }" name="name">
-        <FormItem>
-          <FormLabel>Name</FormLabel>
-          <FormControl>
-            <Input type="text" placeholder="shadcn" v-bind="componentField" />
-          </FormControl>
-          <FormDescription> Products name. </FormDescription>
-          <FormMessage />
-        </FormItem>
-      </FormField>
-      
-      <FormField v-slot="{ componentField }" name="unitPrice">
-        <FormItem>
-          <FormLabel>UnitPrice</FormLabel>
-          <FormControl>
-            <Input type="number" placeholder="0" v-bind="componentField" />
-          </FormControl>
-          <FormDescription> Products price per unit. </FormDescription>
-          <FormMessage />
-        </FormItem>
-      </FormField>
-      <Button type="submit"> Submit </Button>
-    </form>
-    <DataTable :data="localProducts" />
+   
   </div>
 </template>
