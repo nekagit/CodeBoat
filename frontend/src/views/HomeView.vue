b1949429a561fd1f1e51f14c9ad6fd6353dfd311
nekagit <nenad99@hotmail.de>
2024-03-22 21:34:21 +0100

    init

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,65 +1,51 @@
 <script setup lang="ts">
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/lib/registry/new-york/ui/tabs';
 </script>
 
 <template>
   <div class="hidden flex-col md:flex">
     <div class="flex-1 space-y-4 p-8 pt-6">
       <div class="flex items-center justify-between space-y-2">
-        <h2 class="text-3xl font-bold tracking-tight">
-          Dashboard
-        </h2>
-      </div> 
-      <p>
-         Development In Progress - here will be a buying process
-        </p>
-         <!-- <Tabs :default-value="props.entityKey as string" class="w-[400px]">
-          <TabsList>
-            <TabsTrigger :value="AppModule.Product" :disabled="isTabDisabled(AppModule.Product)"
-              >Product</TabsTrigger
-            >
-            <TabsTrigger :value="AppModule.Customer" :disabled="isTabDisabled(AppModule.Customer)"
-              >Customer</TabsTrigger
-            >
-            <TabsTrigger :value="AppModule.Order" :disabled="isTabDisabled(AppModule.Order)"
-              >Invoice</TabsTrigger
-            >
-            <TabsTrigger :value="AppModule.Line" :disabled="isTabDisabled(AppModule.Line)"
-              >InvoiceLine</TabsTrigger
-            >
-          </TabsList>
-          <TabsContent :value="AppModule.Product">
-            <CreateDialogForm
-              :editMode="props.editMode"
-              :item="props.item"
-              :onChange="(x) => props.onChange(x)"
-            />
-          </TabsContent>
-          <TabsContent :value="AppModule.Customer">
-            <CreateDialogForm
-              :editMode="props.editMode"
-              :item="props.item"
-              :onChange="(x) => props.onChange(x)"
-            />
-          </TabsContent>
-          <TabsContent :value="AppModule.Order">
-            <CreateDialogForm
-              :editMode="props.editMode"
-              :item="props.item"
-              :onChange="(x) => props.onChange(x)"
-            />
-          </TabsContent>
-           <TabsContent :value="AppModule.Line">
-            <CreateDialogForm
-              :editMode="props.editMode"
-              :item="props.item"
-              :onChange="(x) => props.onChange(x)"
-            />
-          </TabsContent>
-        </Tabs> -->
+        <h2 class="text-3xl font-bold tracking-tight">Dashboard</h2>
+      </div>
+
+      <Tabs default-value="docusaurus" class="w-full">
+        <TabsList>
+          <TabsTrigger value="docusaurus">Docusaurus</TabsTrigger>
+          <TabsTrigger value="rySport">RYSport</TabsTrigger>
+          <TabsTrigger value="jamal">Jamal</TabsTrigger>
+          <TabsTrigger value="stankovicArt">StankovicArt</TabsTrigger>
+          <TabsTrigger value="stichting">SOP Rotterdam</TabsTrigger>
+        </TabsList>
+        <TabsContent value="docusaurus">
+          <a href="http://docuverse.netlify.app" class="float-right transition-colors hover:text-primary">Docusaurus</a>
+          <iframe src="http://docuverse.netlify.app"></iframe>
+        </TabsContent>
+           <TabsContent value="rySport">
+          <a href="http://docuverse.netlify.app" class="float-right transition-colors hover:text-primary">RYSport</a>
+          <iframe src="http://docuverse.netlify.app"></iframe>
+        </TabsContent>
+        <TabsContent value="jamal">
+          <a href="http://docuverse.netlify.app" class="float-right transition-colors hover:text-primary">Jamal</a>
+          <iframe src="http://docuverse.netlify.app"></iframe>
+        </TabsContent>
+        <TabsContent value="stankovicArt">
+          <a href="https://www.stankovicart.com/" class="float-right transition-colors hover:text-primary">StankovicArt</a>
+          <iframe src="https://www.stankovicart.com/"></iframe>
+        </TabsContent>
+        <TabsContent value="stichting">
+          <a href="https://www.stichtingsoprotterdam.nl" class="float-right transition-colors hover:text-primary">SOP Rotterdam</a>
+          <iframe src="https://www.stichtingsoprotterdam.nl"></iframe>
+        </TabsContent>
+      </Tabs>
     </div>
   </div>
 </template>
 
 <style scoped>
-
+iframe {
+  width: 100%;
+  height: 100vh; /* Set height to 100vh */
+  border: none;
+}
 </style>
