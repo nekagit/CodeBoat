c12217ef7783bfad603a50f5f15a5baff9cbe4ce
nekagit <nenad99@hotmail.de>
2024-03-30 13:27:11 +0100

    init

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,9 @@ async function run() {
     // Stelle die Verbindung zur MongoDB her
     await client.connect();
     console.log("Successfully connected to Atlas");
-
+    app.listen(8080, () => {
+      console.log(`Server is running on port ${8080}`);
+    });
     // await testDB()
     return serverless(app);
   } catch (err) {
@@ -39,85 +41,81 @@ async function run() {
 
 run().catch(console.error);
 
+async function testDB() {
+  // Beispielprodukt für das Einfügen in die Datenbank
+  const product = {
+    name: "John",
+    unitPrice: 1000,
+  };
 
+  // Produkt erstellen
+  const createdProduct = await axios.post(
+    "http://localhost:8080/api/products/",
+    product
+  );
+  const sampleProduct = createdProduct.data; // Use the returned product object
+  console.log("Product created:", sampleProduct);
 
-async function testDB () {
-      // Beispielprodukt für das Einfügen in die Datenbank
-    const product = {
-      name: "John",
-      unitPrice: 1000,
-    };
+  // Beispielkunde für das Einfügen in die Datenbank
+  const customer = await axios.post("http://localhost:8080/api/customers/", {
+    data: { id: sampleInvoice._id },
+  });
 
-    // Produkt erstellen
-    const createdProduct = await axios.post(
-      "http://localhost:8080/api/products/",
-      product
-    );
-    const sampleProduct = createdProduct.data; // Use the returned product object
-    console.log("Product created:", sampleProduct);
+  console.log("Deleted product");
+  console.log("Deleted customer");
+  console.log("Deleted invoice");
+  const createdCustomer = await axios.post(
+    "http://localhost:8080/api/customers/",
+    customer
+  );
+  const sampleCustomer = createdCustomer.data; // Use the returned customer object
+  console.log("Customer created:", sampleCustomer);
 
-    // Beispielkunde für das Einfügen in die Datenbank
-    const customer = await axios.post("http://localhost:8080/api/customers/", {
-      data: { id: sampleInvoice._id },
-    });
+  // Beispielrechnung für das Einfügen in die Datenbank
+  const invoice = {
+    number: 12345,
+    customer: sampleCustomer._id, // Use the customer ID from the created customer
+    date: new Date().toISOString(),
+    invoiceTotal: 500,
+  };
 
-    console.log("Deleted product");
-    console.log("Deleted customer");
-    console.log("Deleted invoice");
-    const createdCustomer = await axios.post(
-      "http://localhost:8080/api/customers/",
-      customer
-    );
-    const sampleCustomer = createdCustomer.data; // Use the returned customer object
-    console.log("Customer created:", sampleCustomer);
+  // Rechnung erstellen
+  const createdInvoice = await axios.post(
+    "http://localhost:8080/api/invoices/",
+    invoice
+  );
 
-    // Beispielrechnung für das Einfügen in die Datenbank
-    const invoice = {
-      number: 12345,
-      customer: sampleCustomer._id, // Use the customer ID from the created customer
-      date: new Date().toISOString(),
-      invoiceTotal: 500,
-    };
+  const sampleInvoice = createdInvoice.data; // Use the returned invoice object
+  console.log("Invoice created:", sampleInvoice);
 
-    // Rechnung erstellen
-    const createdInvoice = await axios.post(
-      "http://localhost:8080/api/invoices/",
-      invoice
-    );
+  // Alle Produkte abrufen
+  const allProducts = await axios.get("http://localhost:8080/api/products/");
+  console.log("Got All Products:", allProducts.data.length);
 
-    const sampleInvoice = createdInvoice.data; // Use the returned invoice object
-    console.log("Invoice created:", sampleInvoice);
+  // Alle Kunden abrufen
+  const allCustomers = await axios.get("http://localhost:8080/api/customers/");
+  console.log("Got All Customers:", allCustomers.data.length);
 
-    // Alle Produkte abrufen
-    const allProducts = await axios.get("http://localhost:8080/api/products/");
-    console.log("Got All Products:", allProducts.data.length);
+  // Alle Rechnungen abrufen
+  const allInvoices = await axios.get("http://localhost:8080/api/invoices/");
+  console.log("Got All Invoices:", allInvoices.data.length);
 
-    // Alle Kunden abrufen
-    const allCustomers = await axios.get(
-      "http://localhost:8080/api/customers/"
-    );
-    console.log("Got All Customers:", allCustomers.data.length);
+  // Produkt löschen
+  await axios.delete("http://localhost:8080/api/products/", {
+    data: { id: sampleProduct._id },
+  });
 
-    // Alle Rechnungen abrufen
-    const allInvoices = await axios.get("http://localhost:8080/api/invoices/");
-    console.log("Got All Invoices:", allInvoices.data.length);
+  // Kunde löschen
+  await axios.delete("http://localhost:8080/api/customers/", {
+    data: { id: sampleCustomer._id },
+  });
 
-    // Produkt löschen
-    await axios.delete("http://localhost:8080/api/products/", {
-      data: { id: sampleProduct._id },
-    });
+  // Rechnung löschen
+  await axios.delete("http://localhost:8080/api/invoices/", {
+    data: { id: sampleInvoice._id },
+  });
 
-    // Kunde löschen
-    await axios.delete("http://localhost:8080/api/customers/", {
-      data: { id: sampleCustomer._id },
-    });
-
-    // Rechnung löschen
-    await axios.delete("http://localhost:8080/api/invoices/", {
-      data: { id: sampleInvoice._id },
-    });
-
-    console.log("Deleted product");
-    console.log("Deleted customer");
-    console.log("Deleted invoice");
+  console.log("Deleted product");
+  console.log("Deleted customer");
+  console.log("Deleted invoice");
 }
