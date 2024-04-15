dbb29ee0901e494a3322b3d51d63aa0e33de75fa
nekagit <nenad99@hotmail.de>
2024-04-15 17:12:08 +0200

    init

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,8 +3,10 @@ import '@/styles/base.css'
 import '@/styles/index.css'
 import '@/styles/main.css'
 import 'animate.css'
+import CircularMenu from '@/components/atoms/CircularMenu.vue'
 </script>
 <template>
+    <CircularMenu :title='"SOP Rotterdam"' />
     <RouterView />
 </template>
 
